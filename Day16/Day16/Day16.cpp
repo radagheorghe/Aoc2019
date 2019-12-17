6db070883c4d8f9aebac0dce993e6a65e1ec03ce
radagheorghe <radagheorghe@github.com>
2019-12-17 22:35:04 +0200

    add day 16 part 2

--- Day16/Day16/Day16.cpp
+++ Day16/Day16/Day16.cpp
@@ -2,19 +2,87 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <assert.h>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+class FFT
+{
+public:
+
+  FFT(const string & aInput)
+  {
+    for (auto ch : aInput)
+      mSignal.push_back(ch - '0');
+  }
+
+  void Transform(int aPhase)
+  {
+    for (int phase = 0; phase < aPhase; phase++)
+    {
+      vector<int> output(mSignal.size());
+      for (size_t i = 0; i < mSignal.size(); i++)
+      {
+        vector<int> pattern = GetPattern(i + 1);
+
+        for (size_t j = 0; j < mSignal.size(); j++)
+          output[i] += mSignal[j] * pattern[j + 1];
+
+        output[i] = abs(output[i]) % 10;
+      }
+      mSignal = output;
+    }
+  }
+
+  const vector<int> & GetSignal()
+  {
+    return mSignal;
+  }
+
+private:
+
+  vector<int> GetPattern(int aIdx)
+  {
+    vector<int> base{ 0, 1, 0, -1 };
+
+    int crBase = 0;
+    vector<int> pattern;
+    while(pattern.size() < mSignal.size() + 1)
+    {
+      for (int i = 0; i < aIdx; i++)
+        pattern.push_back(base[crBase]);
+
+      crBase++;
+      if (crBase > 3)
+        crBase = 0;
+    }
+
+    return pattern;
+  }
+
+  vector<int> mSignal;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  ifstream  myfile;
+
+  myfile.open("..//input.txt");
+  //myfile.open("..//input1.txt");
+    
+  string strLine;
+  getline(myfile, strLine);
+
+  FFT fft(strLine);
+  fft.Transform(100);
+
+  vector<int> signal = fft.GetSignal();
+  for (int i = 0; i < 8; i++)
+    cout << signal[i];
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
